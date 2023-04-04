4ef46225d681e7c224f634fd59276d3f184483e0
lewusqa <125603250+lewusqa@users.noreply.github.com>
2023-04-04 23:46:11 +0200

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,3 +1,19 @@
+resource "google_storage_bucket" "dareit-task7" {
+
+  name          = "dareit-task7"
+  location      = "US"
+  storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "member" {
+  provider = google-beta
+  bucket   = google_storage_bucket.dareit-task7.name
+  role     = "roles/storage.objectViewer"
+  member   = "allUsers"
+}
+
 resource "google_compute_instance" "dareit-vm-ci" {
   name         = "dareit-vm-tf-ci"
   machine_type = "e2-medium"
